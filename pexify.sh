8f390a9ed157aac3052b6e0e17c825d52ce7f341
Loren Carvalho <comradeloren@gmail.com>
2017-03-24 00:25:57 -0500

    Create pexify.sh

---
+++ pexify.sh
@@ -0,0 +1,12 @@
+echo "Make sure your add the following to your .bashrc!"
+echo "..."
+echo "PATH+=":$HOME/bin"
+echo ""
+cd `mktemp -d`
+virtualenv .
+source bin/activate
+pip install pex
+mkdir -p ~/bin
+pex pex -c pex -o ~/bin/pex
+deactivate
+cd ~
